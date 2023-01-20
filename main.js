a49010d14b292237f6c1e12fe0ea2a98d7639cc9
hardik36839 <108940726+hardik36839@users.noreply.github.com>
2023-01-20 19:37:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload()
+{}
+
+function setup()
+{
+    canvas = createCanvas( 400 ,400);
+    canvas.position(110 , 250);
+    vidio = createCapture(VIDEO);
+    vidio.hide();
+}
+function draw()
+{
+    
+}
